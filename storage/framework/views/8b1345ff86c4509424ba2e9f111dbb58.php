e85390cc92699277d20d190bdb20051c8a585fa6
Gita <gitamolyayu152@gmail.com>
2024-06-06 11:32:56 +0700

    test create tamplate

---
+++ storage/framework/views/8b1345ff86c4509424ba2e9f111dbb58.php
@@ -0,0 +1,23 @@
+<?php if (app(\Illuminate\Contracts\Auth\Access\Gate::class)->check('edit-form-category')): ?>
+    <a class="btn btn-sm small btn btn-primary" href="javascript:void(0);"
+    data-url="<?php echo e(route('form-category.edit', $category->id)); ?>" id="edit-form-category"
+        data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-original-title="<?php echo e(__('Edit')); ?>">
+        <i class="ti ti-edit text-white"></i>
+    </a>
+<?php endif; ?>
+<?php if (app(\Illuminate\Contracts\Auth\Access\Gate::class)->check('delete-form-category')): ?>
+    <?php echo Form::open([
+        'method' => 'DELETE',
+        'class' => 'd-inline',
+        'route' => ['form-category.destroy', $category->id],
+        'id' => 'delete-form-' . $category->id,
+    ]); ?>
+
+    <a href="#" class="btn btn-sm small btn btn-danger show_confirm" data-bs-toggle="tooltip" data-bs-placement="bottom"
+        id="delete-form-1" data-bs-original-title="<?php echo e(__('Delete')); ?>">
+        <i class="ti ti-trash text-white"></i>
+    </a>
+    <?php echo Form::close(); ?>
+
+<?php endif; ?>
+<?php /**PATH /Applications/XAMPP/xamppfiles/htdocs/main_file/resources/views/form-category/action.blade.php ENDPATH**/ ?>
